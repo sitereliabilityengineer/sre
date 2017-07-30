b127b3974c32b36df626af68b305cb9f56386e02
Koldo Oteo <koldo.oteo1@gmail.com>
2017-07-30 23:01:02 +0200

    Add files via upload

---
+++ FindPerfIssues/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='',
+    version='0.1.0',
+    description='Find Performance Issues in Linux',
+    long_description=readme,
+    author='Koldo Oteo
+    author_email='koldo.oteo@gmail.com
+    url='https://github.com/SiteReliabilityEngineering/sre/tree/master/FindPerfIssues',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
