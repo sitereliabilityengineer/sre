0572d694c78bf0e9ef8d517a4449fe4cbd4a308b
Koldo Oteo <koldo.oteo1@gmail.com>
2017-07-31 08:47:25 +0200

    Update setup.py

--- FindPerfIssues/setup.py
+++ FindPerfIssues/setup.py
@@ -10,12 +10,12 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='',
+    name='FindPerfIssues',
     version='0.1.0',
     description='Find Performance Issues in Linux',
     long_description=readme,
-    author='Koldo Oteo
-    author_email='koldo.oteo@gmail.com
+    author='Koldo Oteo',
+    author_email='koldo.oteo@gmail.com',
     url='https://github.com/SiteReliabilityEngineering/sre/tree/master/FindPerfIssues',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
